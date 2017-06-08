619284e93a8973dd9cff0f658a317d133c003a04
Sydney Erickson <sydneyerickson@icloud.com>
2017-06-08 10:48:50 -0700

    Test detect header count

--- src/main.py
+++ src/main.py
@@ -5,3 +5,15 @@ print(sys.argv[1])
 with open(sys.argv[1]) as f:
     for line in f.readline:
         print(line)
+
+        isHeader = False
+        headerCount = 0
+
+        if line.startswith("#"):
+            isHeader = True
+            for letter in line:
+                if letter == '#':
+                    headerCount += 1
+                else:
+                    break
+            print(headerCount)
